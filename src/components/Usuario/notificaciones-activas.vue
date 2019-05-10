510a93d5bd1f0cd2d6c4215976dc78af86da376d
Fernando <tbyuyfcjjll@gmail.com>
2019-05-10 15:12:27 -0500

    Creacion. Manejara las notificaciones activas en el navegador actual.

---
+++ src/components/Usuario/notificaciones-activas.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+    div
+        p Notificaciones activas
+        p El id de tu navegador es `{{ $store.state.navegador_hash }}`
+    //
+</template>
+
+<script lang="coffee">
+
+    export default
+        name: "notificaciones-activas"
+    #
+
+</script>
+
+<style scoped lang="sass">
+
+
+    //
+</style>
